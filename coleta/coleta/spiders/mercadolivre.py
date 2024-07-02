005ac9eaf00aaac7cfbc004a0284cced543069f6
Weslan Silva <wesllan90@gmail.com>
2024-07-01 23:10:33 -0300

    finalizando o parse da primeira pagina

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -10,8 +10,21 @@ class MercadolivreSpider(scrapy.Spider):
         products = response.css('div.ui-search-result__content')
 
         for product in products:
+
+            prices = products[0].css('span.andes-money-amount__fraction::text').getall()
+            cents = products[1].css('span.andes-money-amount__cents::text').getall()
+
+
             yield{
-                'brand': product.css('span.ui-search-item__brand-discoverability.ui-search-item__group__element::text').get()
+                'brand': product.css('span.ui-search-item__brand-discoverability.ui-search-item__group__element::text').get(),
+                'name' :product.css('h2.ui-search-item__title::text').get(),
+                'old_price_reais' : prices[0] if len(prices) > 0 else None,
+                'old_price_centavos' : cents[0] if len(cents) > 0 else None,
+                'new_price_reais' : prices[1] if len(prices) > 1 else None,
+                'new_price_centavos' : cents[1] if len(cents) > 1 else None,                
+                'reviews_rating_number' : product.css('span.ui-search-reviews__rating-number::text').get(),
+                'reviews_amount' : product.css('span.ui-search-reviews__amount::text').get()
             }
 
         pass
+#  'reviews_rating_number' : 'reviews_amount' :
